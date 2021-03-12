650aeb927fc42ab95c3c40686d4603376251a20d
AmauryVdt <amaury.vdt@gmail.com>
2021-03-12 23:33:18 +0100

    Tableau des titres wikipedia terminé

--- src/views/Game.vue
+++ src/views/Game.vue
@@ -2,20 +2,28 @@
   <div class="game">
     <div class="wikiTitles">
       <div class="wikiTitles_container">
-        <div class="wikiTitles_unit" v-for="title in wikiTitles.title">
-          <span>{{title}}</span>
+        <div class="wikiTitles_unit" v-for="wikiTitles in wikiTitles">
+          <span>{{wikiTitles.title}}</span>
         </div>
       </div>
     </div>
     <div class="wiki__page">
-      <iframe class="iframe" id="ifr" src="http://localhost:8000/wiki_page?u=https://fr.wikipedia.org/wiki/Cuillère"></iframe>
+      <iframe class="iframe" id="ifr" :src="game.url_start"></iframe>
+    </div>
+    <div class="players">
+      <div class="players_container">
+        <UserInfo class="players_unit" v-for="user in game.users" :key="user.uuid" :user="user" />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import UserInfo from "@/components/UserInfo";
+
 export default {
   name: "Game",
+  components: {UserInfo},
   props: {
     game_id: {
       type: String,
@@ -25,7 +33,7 @@ export default {
   data: function () {
     return {
       game: undefined,
-      wikiTitles: [],
+      wikiTitles: []
     }
   },
   created: function () {
@@ -41,40 +49,40 @@ export default {
             console.log(res.message)
 
           self.game = res.game
-          self.wikiTitles.push(self.game.page_start)
-          console.log(self.game)
+          self.wikiTitles.push({title: self.game.page_start, link: self.game.url_start})
+          self.iframeStartLink = self.game.url_start
+          console.log(self.game.url_start)
         })
         .catch(err => console.log(err))
 
     window.addEventListener('message', event => {
       if (event.origin.startsWith(`http://localhost:8000`)) {
-        this.updateWikiTitles()
+        this.updateWikiTitles(event)
+        console.log(`NEW = ${event.data.new_page.title}`)
+        console.log(`LAST = ${event.data.last_page.title}`)
       }
     })
   },
+  comments: {
+    UserInfo
+  },
   methods: {
-    updateWikiTitles: (event) => {
-      if (Array.isArray(this.wikiTitles)) {
-        // TODO : Il faut reflechir
-          this.wikiTitles.push(event.data.new_page)
+    updateWikiTitles: function (event) {
+      if (event.data.last_page.title === this.wikiTitles[this.wikiTitles.length - 1].title) {
+        this.wikiTitles.push(event.data.new_page)
+      } else if(this.wikiTitles.find(w => w.title === event.data.last_page.title)) {
+        while (this.wikiTitles.length > this.wikiTitles.indexOf(this.wikiTitles.find(w => w.title === event.data.last_page.title)) + 1) {
+          this.wikiTitles.pop() //
+        }
+        this.wikiTitles.push(event.data.new_page)
+      }
+      else {
+        alert("C'est pas ouf de tricher")
       }
     },
   }
 }
 </script>
-
-<!--<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>-->
-<!--<script src="https://code.jquery.com/jquery-migrate-1.4.1.min.js"></script>-->
-
-<!--<script>-->
-
-<!--window.addEventListener('message', event => {-->
-<!--  window.onpopstate = function(event) {-->
-<!--    alert("location: " + document.location + ", state: " + JSON.stringify(event.state));-->
-<!--  };-->
-<!--})-->
-<!--</script>-->
-
 
 <style scoped lang="scss">
 
@@ -87,15 +95,15 @@ export default {
   height: 95vh;
   flex-direction: row;
   justify-content: space-evenly;
-  border: solid orange 3px;
 }
 
+.players,
 .wikiTitles {
   display: flex;
   flex-direction: column;
   //justify-content: space-between;
   align-items: center;
-  width: 20%;
+  width: 15%;
   height: 70%;
   background: rgba(red($dark-grey), green($dark-grey), blue($dark-grey), 0.9);
   border-radius: 75px 30px;
@@ -106,6 +114,7 @@ export default {
   font-family: Roboto, sans-serif;
 }
 
+.players_container,
 .wikiTitles_container {
   display: flex;
   flex-direction: column;
@@ -115,9 +124,9 @@ export default {
   margin-top: 3%;
   margin-bottom: 3%;
   overflow: auto;
-  border: solid pink 3px;
 }
 
+.players_unit,
 .wikiTitles_unit {
   display: flex;
   justify-content: center;
@@ -132,7 +141,7 @@ export default {
 
 .iframe {
   width: 60vw;
-  height: 60vh;
+  height: 80vh;
   //height: 900em;
   box-shadow: 1px 1px 15px 2.5px $dark-grey;
   border-radius: 10px;
